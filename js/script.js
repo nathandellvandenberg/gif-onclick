521d06852a16faa78c8a92d2e59e5263150e2b06
RandyCones <98503979+RandyCones@users.noreply.github.com>
2022-01-28 18:41:59 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -39,20 +39,6 @@
 			function playAudio() {
  			 x.play();
 }
-		var counterVal = 0;
-
-function incrementClick() {
-    updateDisplay(++counterVal);
-}
-
-function resetCounter() {
-    counterVal = 0;
-    updateDisplay(counterVal);
-}
-
-function updateDisplay(val) {
-    document.getElementById("counter-label").innerHTML = val;
-}
 
 
 
